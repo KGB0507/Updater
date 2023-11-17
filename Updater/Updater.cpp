6d3c8883f749b04be11709744029e118ce44cbfb
Кирилл Белозеров <kgb507@mail.ru>
2023-11-17 13:31:48 +0300

    1

--- Updater/Updater.cpp
+++ Updater/Updater.cpp
@@ -6,8 +6,20 @@
 
 /////////////////////////////////////////////////////////////////////////////
 //Notes: 
+// add structure UpdateInfo: version, priority, size etc
+// add dynamic massive AvailableUpdateList which keeps received information about updates
 /////////////////////////////////////////////////////////////////////////////
 
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
 
 #define _CRT_SECURE_NO_WARNINGS
 
@@ -18,36 +30,31 @@
 #define UNDERCONSTR "Under construction"
 #define DEBUG 1.0
 
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 
 
 
-const std::string SERVERADDR = "127.0.0.1"; // IP-адрес сервера
+
+const string SERVERADDR = "127.0.0.1"; // IP-адрес сервера
 const int SERVERPORT = 8080; // Порт сервера
 
 
-
-int main(int argc, char* argv[])
+int main() 
 {
-    // Параметры номера приложения и версии
-    int appNumber;
-    std::string appVersion;
+    setlocale(LC_ALL, "Rus");
 
-    std::cout << "Введите номер приложения: ";
-    std::cin >> appNumber;
-
-    std::cout << "Введите версию приложения (до 3 чисел, разделенных точкой): ";
-    std::cin >> appVersion;
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) 
+    {
+        cerr << "Ошибка инициализации winsock" << endl;
+        return 1;
+    }
 
     // Создание сокета
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == -1) {
-        std::cerr << "Ошибка создания сокета" << std::endl;
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) 
+    {
+        cerr << "Ошибка создания сокета" << endl;
+        WSACleanup();
         return 1;
     }
 
@@ -55,23 +62,42 @@ int main(int argc, char* argv[])
     sockaddr_in serverAddress{};
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(SERVERPORT);
-    if (inet_pton(AF_INET, SERVERADDR.c_str(), &serverAddress.sin_addr) <= 0) {
-        std::cerr << "Ошибка адреса сервера" << std::endl;
+    if (inet_pton(AF_INET, SERVERADDR.c_str(), &(serverAddress.sin_addr.s_addr)) <= 0) 
+    {
+        cerr << "Ошибка адреса сервера" << endl;
+        closesocket(clientSocket);
+        WSACleanup();
         return 1;
     }
 
     // Подключение к серверу
-    if (connect(clientSocket, reinterpret_cast<const sockaddr*>(&serverAddress), sizeof(serverAddress)) == -1) {
-        std::cerr << "Ошибка подключения к серверу" << std::endl;
+    if (connect(clientSocket, reinterpret_cast<const sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) 
+    {
+        cerr << "Ошибка подключения к серверу" << endl;
+        closesocket(clientSocket);
+        WSACleanup();
         return 1;
     }
 
+    // Параметры номера приложения и версии
+    int appNumber;
+    string appVersion;
+
+    cout << "Введите номер приложения: ";
+    cin >> appNumber;
+
+    cout << "Введите версию приложения (до 3 чисел, разделенных точкой): ";
+    cin >> appVersion;
+
     // Формирование запроса
-    std::string request = std::to_string(appNumber) + " " + appVersion;
+    string request = to_string(appNumber) + " " + appVersion;
 
     // Отправка запроса
-    if (send(clientSocket, request.c_str(), request.length(), 0) == -1) {
-        std::cerr << "Ошибка отправки запроса" << std::endl;
+    if (send(clientSocket, request.c_str(), request.length(), 0) == SOCKET_ERROR) 
+    {
+        cerr << "Ошибка отправки запроса" << endl;
+        closesocket(clientSocket);
+        WSACleanup();
         return 1;
     }
 
@@ -79,19 +105,23 @@ int main(int argc, char* argv[])
     char buffer[10];
     memset(buffer, 0, sizeof(buffer));
 
-    if (recv(clientSocket, buffer, sizeof(buffer), 0) == -1) {
-        std::cerr << "Ошибка получения ответа" << std::endl;
+    if (recv(clientSocket, buffer, sizeof(buffer), 0) == SOCKET_ERROR) 
+    {
+        cerr << "Ошибка получения ответа" << endl;
+        closesocket(clientSocket);
+        WSACleanup();
         return 1;
     }
 
     // Закрытие сокета
-    close(clientSocket);
+    closesocket(clientSocket);
+    WSACleanup();
 
     // Вывод результата
-    std::string response = std::string(buffer);
+    string response = std::string(buffer);
     bool hasUpdates = (response == "true");
 
-    std::cout << "Наличие обновлений: " << (hasUpdates ? "true" : "false") << std::endl;
+    cout << "Наличие обновлений: " << (hasUpdates ? "true" : "false") << endl;
 
     return 0;
 }
